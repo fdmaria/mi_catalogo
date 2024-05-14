72589561838dc0bc14926428aa970c066d9022eb
fdmaria <fdmaria.velasquez@gmail.com>
2024-05-13 23:57:50 -0500

    Se moodifica pantalla para mostrar el banner

--- src/components/PortadaCatalogo.jsx
+++ src/components/PortadaCatalogo.jsx
@@ -1,11 +1,14 @@
+import "./PortadaCatalogo.scss"
 import { useState } from "react"
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 
 function PortadaCatalogo() {
-    let [imagenSeleccionada, setImagenSeleccionada] = useState("https://picsum.photos/id/237/200/200")
+    let [imagenSeleccionada, setImagenSeleccionada] = useState("https://picsum.photos/id/250/480/200")
     let galeria = [
-        "https://picsum.photos/id/235/200/200",
-        "https://picsum.photos/id/278/200/200",
-        "https://picsum.photos/id/217/200/200"
+        "https://picsum.photos/id/235/580/300",
+        "https://picsum.photos/id/278/580/300",
+        "https://picsum.photos/id/217/580/300"
     ]
 
     const cambiarimagen = function() {
@@ -14,9 +17,16 @@ function PortadaCatalogo() {
         setImagenSeleccionada(galeria[indice])
     }
     return (
-         <div>
-            <img src={imagenSeleccionada} alt="" />
-            <button onClick={cambiarimagen}>cambiar</button>
+         <div className="banner-portada"> 
+            <img className="banner-portada__img-banner" src={imagenSeleccionada} alt="" />
+            <div className="banner-portada__btn-controls">
+                {/* <button className="" onClick={cambiarimagen}><MdKeyboardArrowLeft /></button>            */}
+                {/* <button className="" onClick={cambiarimagen}><MdKeyboardArrowRight /></button>             */}
+                <a href="#" className="banner-portada__btn-controls__arrow" onClick={cambiarimagen}><MdKeyboardArrowLeft /></a>
+                <div className="banner-portada__btn-controls__copy"> caja de contenido</div>
+                <a href="#" className="banner-portada__btn-controls__arrow" onClick={cambiarimagen}><MdKeyboardArrowRight /></a>          
+
+            </div>
          </div>
     )
 }
